9f483807133541400530d9dd98f757e7fa2458a9
NafiGit <150414891+Nahfid-Tellis@users.noreply.github.com>
2024-10-09 15:09:48 +0530

    chore: added some quesions

---
+++ 128.longest-consecutive-sequence.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=128 lang=javascript
+ *
+ * [128] Longest Consecutive Sequence
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    console.log(nums);
+};
+// @lc code=end
+
