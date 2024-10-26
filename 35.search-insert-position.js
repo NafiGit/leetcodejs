6ee83bd4305dc0c13b92f053c350f47687b721a8
NafiGit <150414891+Nahfid-Tellis@users.noreply.github.com>
2024-10-26 18:27:50 +0530

    add: two new easy questions for confidence boost

--- 35.search-insert-position.js
+++ 35.search-insert-position.js
@@ -11,7 +11,38 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+    console.log(nums);
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (true) {
+        let mid = Math.floor( (l + r) / 2 );
+
+        console.log(mid);
     
+        if ( nums.length === 0 ) {
+            return 0;
+        } 
+    
+        if (l > r) {
+            return l;
+        }
+    
+        if ( nums[mid] > target ) {
+            r = mid - 1;
+            console.log (r);
+        } else if ( nums[mid] < target ) {
+            l = mid + 1;
+            console.log(l)
+        } else if ( nums[mid] === target ) {
+            return mid;
+        }
+    
+        console.log ("end", l, r, mid);    
+
+    }
+
+
 };
 // @lc code=end
 
--- 58.length-of-last-word.js
+++ 58.length-of-last-word.js
@@ -10,7 +10,14 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
+    console.log( s );
+
+    let s2 = s.trim()
+    let s3 = s2.split(' ')
+    console.log(s3)
+    let s4 = s3[s3.length-1]
+
+    return s4.length
 };
 // @lc code=end
 
